915b6f8c681b8bb8292191a9887d63f10f845188
Kerry Buckley <kerryjbuckley@gmail.com>
2022-02-12 17:41:22 +0000

    Filter out words with common letters for entirely wrong guesses

--- lib/wordle.ex
+++ lib/wordle.ex
@@ -1,5 +1,13 @@
 defmodule Wordle do
-  def possible_words(dictionary, _guesses, _answer) do
-    dictionary
+  def possible_words(dictionary, guesses, _answer) do
+    Enum.filter(dictionary, &no_common_letters?(&1, guesses))
+  end
+
+  defp no_common_letters?(word, guesses) do
+    not Enum.any?(guesses, &common_letters?(&1, word))
+  end
+
+  defp common_letters?(word_1, word_2) do
+    Enum.any?(String.codepoints(word_1), &(&1 in String.codepoints(word_2)))
   end
 end
