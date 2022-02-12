1402d1ce2b7facaa8268349e6657a918142d32ec
Kerry Buckley <kerryjbuckley@gmail.com>
2022-02-12 14:59:20 +0000

    Add failing test for no guesses yet

--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -1,3 +1,9 @@
 defmodule WordleTest do
   use ExUnit.Case
+
+  describe "Wordle.possible_words/3" do
+    test "returns all words if there are no guesses" do
+      assert Wordle.possible_words(["hello", "world"], [], "hello") == ["hello", "world"]
+    end
+  end
 end
