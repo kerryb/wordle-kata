96bea4894aa73440a85e0630d52b659af1adec61
Kerry Buckley <kerryjbuckley@gmail.com>
2022-02-12 17:20:06 +0000

    Add failing test for entirely wrong guesses

--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -5,5 +5,13 @@ defmodule WordleTest do
     test "returns all words if there are no guesses" do
       assert Wordle.possible_words(["hello", "world"], [], "hello") == ["hello", "world"]
     end
+
+    test "returns words that don't share any letters with any guesses if the guesses don't match any letters in the answer" do
+      assert Wordle.possible_words(
+               ["hello", "world", "quack", "moist", "glaze"],
+               ["drink", "stump"],
+               "hello"
+             ) == ["hello", "glaze"]
+    end
   end
 end
